25bc3862823a5696d6e3737de3d59e98b4554fc2
Amila Maharachchi <amila@WSO2s-MacBook-Pro-4.local>
2017-12-14 17:21:45 +0530

    Commenting out the basic auth stuff

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var basicAuth = require('express-basic-auth');
+//var basicAuth = require('express-basic-auth');
 var app = express();
 
-app.use(basicAuth({
-    users: { 'admin': 'admin' }
-}))
+//app.use(basicAuth({
+//    users: { 'admin': 'admin' }
+//}))
 
 app.set('port', (process.env.PORT || 5000));
 
